8c0c9360a8145b20d497221fc481096d18452c3e
WhippyCleric <matthewstuartdunn@gmail.com>
2018-11-03 16:00:44 +0000

    Add mroe table seating

--- 5CardAI/game_area/seat.py
+++ 5CardAI/game_area/seat.py
@@ -7,9 +7,11 @@ class Seat:
     
     number = -1;
     player = None;
+    isFree = True;
     
     def __init__(self, number):
         self.number = number;
     
     def seatPlayer(self, player):
+        self.isFree = False;
         self.player = player;
--- 5CardAI/game_area/table.py
+++ 5CardAI/game_area/table.py
@@ -4,7 +4,7 @@ Created on 3 Nov 2018
 @author: mdunn
 '''
 from game_area.seat import Seat;
-
+from cardai_exceptions.no_free_seat import NoFreeSeatException;
 class Table:
     
     seats = []
@@ -12,4 +12,18 @@ class Table:
     def __init__(self, numberOfSeats):
          
         for i in range(numberOfSeats):
-            self.seats.append(Seat(i))
+            self.seats.append(Seat(i));
+            
+            
+    def seatPlayer(self, player):
+        freeSeat = self.findFreeSeat();
+        if freeSeat != None:
+            freeSeat.seatPlayer(player);
+        else:
+            raise NoFreeSeatException();
+        
+    def findFreeSeat(self):
+        for seat in self.seats:
+            if seat.isFree:
+                return seat;
+        return None;
